daf8f01cfef4d6b9e80feb34928d54b00f25ee11
Keyoungg2 <156037472+Keyoungg2@users.noreply.github.com>
2024-01-19 00:16:45 -0600

    Create K.Young_projsetup.py

---
+++ K.Young_projsetup.py
@@ -0,0 +1,7 @@
+''' This module provides functions for creating a series of project folders. '''
+
+#Imported Python libraries 
+import math
+import statistics
+import Key_2_Analytics.py
+import pathlib
